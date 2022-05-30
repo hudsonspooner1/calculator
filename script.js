2f137af53e16b74bd1e9fe2bd9ed29f2f124e0d7
Hudson Spooner <hudson.spooner1@gmail.com>
2022-05-30 14:49:50 +0800

    added buttons and display to html with some minor UI changes with css. JS has very primitive maths functions.

---
+++ script.js
@@ -0,0 +1,34 @@
+let nOne = 2;
+let nTwo = 5;
+answer = 0;
+
+function add() {
+    answer = nOne + nTwo;
+    return answer;
+}
+
+function subtract() {
+    answer = nOne - nTwo;
+    return answer;
+}
+
+function divide() {
+    answer = nOne / nTwo;
+    return answer;
+}
+
+function multiply() {
+    answer = nOne * nTwo;
+    return answer;
+}
+
+
+function display() {
+    document.getElementById("bOne").onclick = 
+}
+
+function operate() {
+
+}
+
+operate();
